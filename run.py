d5def9a88349e61bf8d11b0cd9a89975d97d1d8c
Rayhebe <hebekuranyi@gmail.com>
2024-09-01 19:16:54 +0300

    Update run.py

--- run.py
+++ run.py
@@ -24,9 +24,9 @@ class WebServer():
 
 # BOT SETTINGS
 bot_file_name = "main"
-bot_class_name = "bot"
+bot_class_name = "bottest2"
 room_id = "66bad059afeca0c24b497205"
-bot_token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"
+bot_token = "6fa44d28975b94f1ba9c206cd930e8f0965bf80d4b5ed4fec4cbd6b4818cbd8a"
 
 my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
 
