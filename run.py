668e4f299a0a350f487ea4e2edbde022ace5ce73
Rayhebe <hebekuranyi@gmail.com>
2024-09-01 19:29:15 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-import sys
-import os
-import time
-import traceback
-from importlib import import_module
+from flask import Flask
+from threading import Thread
 from highrise.__main__ import *
+import time
+
 
 class WebServer():
 
@@ -15,27 +14,36 @@ class WebServer():
       return "Funcionando"
 
   def run(self) -> None:
-    self.app.run(host='0.0.0.0', port=8010)
+    self.app.run(host='0.0.0.0', port=8089)
 
   def keep_alive(self):
     t = Thread(target=self.run)
     t.start()
 
 
-# BOT SETTINGS
-bot_file_name = "main"
-bot_class_name = "bottest2"
-room_id = "66bad059afeca0c24b497205"
-bot_token = "6fa44d28975b94f1ba9c206cd930e8f0965bf80d4b5ed4fec4cbd6b4818cbd8a"
+class RunBot():
+  room_id = "66bad059afeca0c24b497205"
+  bot_token = "6fa44d28975b94f1ba9c206cd930e8f0965bf80d4b5ed4fec4cbd6b4818cbd8a"
+  bot_file = "main"
+  bot_class = "Bot"
 
-my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
+  def __init__(self) -> None:
+    self.definitions = [
+        BotDefinition(
+            getattr(import_module(self.bot_file), self.bot_class)(),
+            self.room_id, self.bot_token)
+    ]  # More BotDefinition classes can be added to the definitions list
 
-while True:
-    try:
-        definitions = [my_bot]
-        arun(main(definitions))
-    except Exception as e:
-        print(f"An exception occurred: {e}")
-        traceback.print_exc()
-    time.sleep(5)
+  def run_loop(self) -> None:
+    while True:
+      try:
+        arun(main(self.definitions))
 
+      except Exception as e:
+        print("Error: ", e)
+        time.sleep(5)
+
+if __name__ == "__main__":
+  WebServer().keep_alive()
+
+  RunBot().run_loop()
