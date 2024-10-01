bc2e2c1461fd27cf9f13fdc33f02480142d04e94
Rayhebe <hebekuranyi@gmail.com>
2024-10-01 05:07:29 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from highrise.__main__ import *
 # BOT SETTINGS
 bot_file_name = "MGBot"
 bot_class_name = "MGBot"
-room_id = "66bad059afeca0c24b497205"
+room_id = "66d2726b2e80dd1f614c4dbb"
 bot_token ="432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"
 
 my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
